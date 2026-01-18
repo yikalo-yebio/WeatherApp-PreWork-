ca7d0bf3a09e97e151b0248a5859c69dcb9cd49d
Yikalo Yebio <yikaloye@gmail.com>
2026-01-17 21:19:10 -0600

    final stage of backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,2 +1,21 @@
 let cityTxtFld = document.getElementById("city-txtfld");
 let searchBtn = document.getElementById("search-btn");
+
+searchBtn.addEventListener("click", fetchData);
+
+async function fetchData() {
+    
+    const cityName = cityTxtFld.ariaValueMax.trim();
+
+    if (!cityName) {
+        alert("Please enter a city name");
+        return;
+    }
+
+    try {
+        const response = await fetch(`/weather?city=${cityName}`);
+        const data = await response.json();
+    } catch (error) {
+        console.error("Error fetching weather data:", error);
+    }
+}
--- server.js
+++ server.js
@@ -1,28 +1,38 @@
 import express from "express";
 import dotenv from "dotenv";
- 
+import fetch from "node-fetch";
+
 dotenv.config();
 
- const app = express();
- const PORT = 3000;
+const app = express();
+const PORT = 3000;
 
- app.use(express.static("public"));
 
- app.get("weather", async (req, res) => {
-    const city = req.query.city;
+app.use(express.static("frontend"));
 
-    if (!city) {
-        return res.status(400).json({ error: "city is required"});
+app.get("/weather", async (req, res) => {
+  const city = req.query.city;
+
+  if (!city) {
+    return res.status(400).json({ error: "City is required" });
+  }
+
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (!response.ok) {
+      return res.status(response.status).json({ error: data.message });
     }
 
-    try {
-        const response = await fetch(
-         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}`
-        );
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch weather data" });
+  }
+});
 
-        const data = await response.json();
-        res.json(data);
-    } catch (err) {
-        res.status(500).json({error: "Failed to fetch weather data"});
-    }
- });
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
